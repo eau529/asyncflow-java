54e4d1b222d776cec9a6922cf1f0a37b42020900
witt <mymeek@163.com>
2023-08-22 17:10:10 +0800

    整合 flowsvr 和 worker

---
+++ worker/src/main/java/com/zdf/worker/WorkerApplication.java
@@ -0,0 +1,22 @@
+package com.zdf.worker;
+
+import com.zdf.worker.boot.AppLaunch;
+import com.zdf.worker.boot.Launch;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkerApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkerApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Launch l = new AppLaunch();
+        // 启动worker
+        l.start();
+    }
+}
